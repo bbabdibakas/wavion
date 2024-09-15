2d41a4bc782b5743fec326556df84669fba6fc4a
Bekzat Abdibakas <babdibakas@yandex.kz>
2024-09-15 17:21:18 +0500

    feat: installed react, webpack-dev-server

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,10 +1,11 @@
 import path from 'path'
 import HTMLWebpackPlugin from 'html-webpack-plugin'
 import webpack from 'webpack';
+import webpackDevServer from 'webpack-dev-server';
 
 const config: webpack.Configuration = {
     mode: 'development',
-    entry: path.resolve(__dirname, 'src', 'index.ts'),
+    entry: path.resolve(__dirname, 'src', 'index.tsx'),
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: '[name].[contenthash:8].js',
@@ -28,6 +29,10 @@ const config: webpack.Configuration = {
     resolve: {
         extensions: ['.tsx', '.ts', '.js'],
     },
+    devServer: {
+        port: 3000,
+        open: true
+    }
 }
 
 export default config;
