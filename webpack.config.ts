74ab8420bdc004d1630fa8bdca08c97465584f8c
Bekzat Abdibakas <babdibakas@yandex.kz>
2024-09-15 22:43:52 +0500

    fix: decomposition webpack config

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,74 +1,26 @@
-import path from 'path'
-import HTMLWebpackPlugin from 'html-webpack-plugin'
 import webpack from 'webpack';
-import webpackDevServer from 'webpack-dev-server';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import path from 'path';
+import { buildWebpackConfig } from './config/build/buildWebpackConfig';
+import { BuildEnv, BuildPaths } from './config/build/types/config';
 
-interface IEnv {
-    mode: 'development' | 'production'
-}
-
-export default (env: IEnv) => {
-    const mode = env.mode || 'development'
-    const isDev = mode === 'development'
-    
-    const config: webpack.Configuration = {
-        mode: 'development',
+export default (env: BuildEnv) => {
+    const paths: BuildPaths = {
         entry: path.resolve(__dirname, 'src', 'index.tsx'),
-        output: {
-            path: path.resolve(__dirname, 'build'),
-            filename: '[name].[contenthash:8].js',
-            clean: true
-        },
-        plugins: [
-            new HTMLWebpackPlugin({
-                template: path.resolve(__dirname, 'public', 'index.html'),
-            }),
-            new MiniCssExtractPlugin({
-                filename: 'css/[name].[contenthash:8].css',
-                chunkFilename: 'css/[name].[contenthash:8].css',
-            }),
-        ],
-        devtool: 'inline-source-map',
-        module: {
-            rules: [
-                {
-                    test: /\.s[ac]ss$/i,
-                    use: [
-                        // Creates `style` nodes from JS strings
-                        isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-                        // Translates CSS into CommonJS
-                        {
-                            loader: 'css-loader',
-                            options: {
-                                modules: {
-                                    auto: (resPath: string) => Boolean(resPath.includes('.module.')),
-                                    localIdentName: isDev
-                                        ? '[path][name]__[local]--[hash:base64:5]'
-                                        : '[hash:base64:8]',
-                                },
-                            },
-                        },
-                        // Compiles Sass to CSS
-                        "sass-loader",
-                    ],
-                },
-                {
-                    test: /\.tsx?$/,
-                    use: 'ts-loader',
-                    exclude: /node_modules/,
-                },
-            ],
-        },
-        resolve: {
-            extensions: ['.tsx', '.ts', '.js'],
-        },
-        devServer: {
-            port: 3000,
-            open: true,
-            historyApiFallback: true
-        }
-    }
+        build: path.resolve(__dirname, 'build'),
+        html: path.resolve(__dirname, 'public', 'index.html'),
+    };
 
-    return config
-}
+    const mode = env.mode || 'development';
+    const port = env.port || 3000;
+
+    const isDev = mode === 'development';
+
+    const config: webpack.Configuration = buildWebpackConfig({
+        mode,
+        paths,
+        isDev,
+        port,
+    });
+
+    return config;
+};
